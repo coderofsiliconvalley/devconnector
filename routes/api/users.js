bb9043a0f8397f3881caa1570b8a6fd14b0f35a1
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-25 14:27:05 -0400

    removing last commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-// @route GET api/userss
+// @route GET api/users
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
 module.exports = router;
