084594ac9691e25c18b6dced2d13bd759dcd7a38
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-26 15:31:34 -0400

    Post create route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const Post = require("../../models/Post");
-const Post = require("../../models/Post");
 const validatePostInput = require("../../validation/post");
 
 // @route GET api/posts
@@ -16,10 +15,17 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
-      avatar: req.body.name,
+      avatar: req.body.avatar,
       user: req.user._id
     });
 
