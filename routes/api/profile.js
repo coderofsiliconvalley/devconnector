4f2f5487016fadd19ede1f6102e8ac4525b1125b
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-26 14:06:05 -0400

    Finished Profile Api routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -9,6 +9,8 @@ const Profile = require("../../models/Profile");
 // Load Profile User
 const User = require("../../models/User");
 const validateProfileInput = require("../../validation/profile");
+const validateExperienceInput = require("../../validation/experience");
+const validateEducationInput = require("../../validation/education");
 
 // @route GET api/profile
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
@@ -133,6 +135,117 @@ router.post(
           new Profile(profileFields).save().then(profile => res.json(profile));
         });
       }
+    });
+  }
+);
+
+//POST api/profile/experience
+router.post(
+  "/experience",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+    console.log(errors);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    Profile.findOne({ user: req.user._id }).then(profile => {
+      const newExp = {
+        title: req.body.title,
+        company: req.body.company,
+        location: req.body.location,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+      // add to exp array
+      profile.experience.unshift(newExp);
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+//POST api/profile/education
+router.post(
+  "/education",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateEducationInput(req.body);
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    Profile.findOne({ user: req.user._id }).then(profile => {
+      const newEdu = {
+        school: req.body.school,
+        degree: req.body.degree,
+        fieldofstudy: req.body.fieldofstudy,
+        from: req.body.from,
+        to: req.body.to,
+        current: req.body.current,
+        description: req.body.description
+      };
+      // add to education array
+      profile.education.unshift(newEdu);
+      profile.save().then(profile => res.json(profile));
+    });
+  }
+);
+
+//DELETE api/profile/experience/:exp_id
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user._id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.exp_id);
+
+        // Splice out of array
+        profile.experience.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+//DELETE api/profile/education/:exp_id
+router.delete(
+  "/education/:edu_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user._id })
+      .then(profile => {
+        // Get remove index
+        const removeIndex = profile.education
+          .map(item => item.id)
+          .indexOf(req.params.edu_id);
+
+        // Splice out of array
+        profile.education.splice(removeIndex, 1);
+
+        // Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// Delete user and profile
+//DELETE api/profile/education/:exp_id
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user._id }).then(() => {
+      User.findOneAndRemove({ _id: req.user._id }).then(() =>
+        res.json({ success: true })
+      );
     });
   }
 );
---
+++ validation/education.js
@@ -0,0 +1,29 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validateExperienceInput(data) {
+  let errors = {};
+  console.log(data.title);
+  data.school = !isEmpty(data.school) ? data.school : "";
+  data.degree = !isEmpty(data.degree) ? data.degree : "";
+  data.from = !isEmpty(data.from) ? data.from : "";
+  data.fieldofstudy = !isEmpty(data.fieldofstudy) ? data.fieldofstudy : "";
+
+  if (Validator.isEmpty(data.school)) {
+    errors.school = "School field is required";
+  }
+  if (Validator.isEmpty(data.degree)) {
+    errors.degree = "Company field is required";
+  }
+  if (Validator.isEmpty(data.from)) {
+    errors.from = "from field is required";
+  }
+  if (Validator.isEmpty(data.fieldofstudy)) {
+    errors.fieldofstudy = "fieldofstudy date field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
