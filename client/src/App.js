be35e355cf2c09cc0fa34067d7548383a3a2d4ec
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-28 13:59:54 -0400

    created createStore()

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,26 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
+const store = createStore(() => [], {}, applyMiddleware());
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" component={Login} />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
