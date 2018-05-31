2088d6e8aa3b88acd49a3fd911d4011f150b40b1
Vasanth Neppalli <neppallv@my.canisius.edu>
2018-05-31 13:48:22 -0400

    starting logiand conditional tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
@@ -9,6 +12,15 @@ import Login from "./components/auth/Login";
 import { Provider } from "react-redux";
 import store from "./store";
 
+// Check for token
+if (localStorage.jwtToken) {
+  // Set the auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 class App extends Component {
   render() {
     return (
