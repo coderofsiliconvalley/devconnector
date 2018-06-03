1cb7ec7ed02f940192489982c56a6b7d7bd3238e
Vasanth Anuraag Neppalli <neppallv@canisius.edu>
2018-06-03 15:31:50 -0400

    starting to work on Profile

---
+++ client/src/components/profile/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ProfileHeader from "./ProfileHeader";
+import ProfileAbout from "./ProfileAbout";
+import ProfileCreds from "./ProfileCreds";
+import ProfileHeader from "./ProfileGithub";
+
+class Profile extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Profile;
